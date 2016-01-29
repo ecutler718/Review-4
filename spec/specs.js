f8f6a1b4f79778ab236cb53acd2b05a3207b9293
emmanuel cutler <student@epicodus.com>
2016-01-29 11:13:48 -0800

    add specs but first doesnt work

---
+++ spec/specs.js
@@ -0,0 +1,18 @@
+describe('Pizza', function() {
+	it("creates a new Pizza with the given properties", function() {
+		var testPizza = new Pizza("Small", "Vegan Cheese", true);
+		expect(testPizza.Size).to.equal("Small");
+		expect(testPizza.Cheese).to.equal("Vegan Cheese");
+		expect(testPizza.topping).to.equal(true);
+	});
+  it("adds the price method to a pizza - test 1", function() {
+		var testPizza = new Pizza("Small", "Vegan Cheese", true);
+		expect(testPizza.price()).to.equal(19);
+	});
+
+	it("adds the price method to a pizza - test 2", function() {
+		var testPizza = new Pizza("Small", "Vegan Cheese", false);
+		expect(testPizza.price()).to.equal(14);
+	});
+
+});
